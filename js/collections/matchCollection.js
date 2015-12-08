c79e339d941452b75f14e49f92433b0eda5fd6ff
Nicholas Reynolds <sonshine_80@yahoo.com>
2015-12-07 18:53:44 -0800

    Adds collections and models

--- js/collections/matchCollection.js
+++ js/collections/matchCollection.js
@@ -15,3 +15,31 @@ matchCollection.fetch ({
     console.log('error', err);
   }
 });
+
+$("#setMatch").on('click', function() {
+  matchAdd = new matchContainer();
+  matchAdd.set({
+    title: $("#").val(),
+    description: $("#").val(),
+    park: $("#").val(),
+    sport: $("#").val(),
+    skill_level: $("#").val(),
+    date_time: $("#").val(),
+    players: $("#").val()
+  })
+  matchAdd.save(null, {
+    url: 'https://skill-match.herokuapp.com/api/matches/',
+    success: function(resp) {
+      console.log("success", resp);
+    },
+    error: function(err) {
+      concole.log("error", err);
+    }
+  });
+      $("#").val("");
+      $("#").val("");
+      $("#").val("");
+      $("#").val("");
+      $("#").val("");
+      $("#").val("");
+})
