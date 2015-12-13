be256df16dbef31b4979a83e896b017884f9c460
Nicholas Reynolds <sonshine_80@yahoo.com>
2015-12-13 15:59:54 -0800

    Style the match detail page

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ require("../css/index.css");
 require("../css/normal.css");
 require("../css/main.css");
 require("../css/createMatch.css");
+require("../css/match.css");
 
 $(document).ready(function(){
   var Router = require('./router.js');
