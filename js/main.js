f3949f1242735fb151bdadad14937c22e6d6edd9
Nicholas Reynolds <sonshine_80@yahoo.com>
2015-12-15 12:00:54 -0800

    Adds user name and a welcome when signed in

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
           var username = $("#username").val();
           var password = $("#password").val();
           var homeBtn = document.createElement('a');
-          var link = document.createTextNode(username);
+          var link = document.createTextNode("Hello, " + username);
           homeBtn.appendChild(link);
 			  homeBtn.setAttribute('href', '/home/'+username);
           $.ajax({
